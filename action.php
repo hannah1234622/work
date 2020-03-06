e99df6c96299ae01cdcf5d1ec62aa00e77e7cbb2
unknown <yu-hua@bb.com>
2020-03-06 17:35:09 +0800

    work2

--- action.php
+++ action.php
@@ -1,7 +1,16 @@
-
-<?php 
-
-echo $_POST["city"];
-echo $_POST["region"];
-
+<?PHP
+    $url = "https://data.nhi.gov.tw/Datasets/Download.ashx?rid=A21030000I-D50001-001&l=https://data.nhi.gov.tw/resource/mask/maskdata.csv";
+    //function getURL($url){ 
+        $ch = curl_init();
+        curl_setopt($ch,CURLOPT_URL,$url);
+        curl_setopt($ch,CURLOPT_HEADER, 1);
+        curl_setopt($ch,CURLOPT_RETURNTRANSFER, 1);
+        //curl_setopt($ch,CURLOPT_HTTPHEADER, array('Host:google.com'));
+        //curl_setopt($ch,CURLOPT_VERIFYHOST,FALSE);
+        $data = curl_exec($ch); 
+        curl_close($ch); 
+        var_dump($data);
+        //return $ret;
+    //} 
+    
 ?>
--- test.php
+++ test.php
@@ -1,43 +1,3 @@
-<?php
-    $county = array("請選擇","台北市","基隆市","新北市","宜蘭縣","桃園市","新竹市","新竹縣","苗栗縣","台中市","彰化縣","南投縣","嘉義市","嘉義縣","雲林縣","台南市","高雄市","澎湖縣","金門縣","屏東縣","台東縣","花蓮縣","連江縣");
-    $ch = curl_init();//初始化curl 令變數為 $curlobj
-    curl_setopt($ch, CURLOPT_URL,"https://quality.data.gov.tw/dq_download_json.php?nid=116285&md5_url=2150b333756e64325bdbc4a5fd45fad1");
-    //向網址發出請求 訪問該網頁
-    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);//執行後不打印出來
-    //設置Cookie
-    date_default_timezone_set('Asia/Taipei'); //設置台北時區
-    curl_setopt($ch,CURLOPT_COOKIESESSION,TRUE); //設為 TRUE 時 開啟COOKIE
-    curl_setopt($ch,CURLOPT_HEADER,0);
-    curl_setopt($ch,CURLOPT_HTTPHEADER,array("content-type: application/x-www-form-urlencoded;charset=UTF-8"));
-    curl_exec($ch);//執行curl
-    $output=curl_exec($ch);  // 執行 curl 並令一個變數 $output
-    curl_close($ch); // 關閉curl
-    echo '<pre>';
-    $arrayName = json_decode($output,true);//json_decode是一個方程式 對JSON格式的字符進行解碼
-    
-    $city = $_POST["city"];
-    $region = $_POST["region"];
- 
-    var_dump($county[$city]);
-
-    function search($arrayName,$region){ //search方程式匯入$arrayName陣列及$region POST的值
-        $arr=$result=array();
-        foreach ($arrayName as $key => $value) { //取得$arrayName的key值
-            foreach ($value as $val) { //求出key裡面的value值
-                if(strstr($val,$region)){ //若value符合搜尋值
-                    array_push($arr,$key); //放入arr陣列
-                }
-            }
-        }
-        foreach ($arr as $key => $value) { //將$arr的key值求出
-            if(array_key_exists($value,$arrayName)){//若key值存在
-                array_push($result,$arrayName[$value]);//將key裡面的值放入array
-            }
-        }
-        return $result;
-    }
-
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -55,7 +15,7 @@
         <div style="background-color: white;width: 80%;margin-left: 10%;padding: 30px;margin-top: 5%;">
             <h1 style="color: rgb(4, 129, 255);font-weight: bold;">口罩供需即時資訊查詢</h1>
             <hr>
-            <ol style="border: black 1px solid;position: relative;left: 15%;width: 70%;">
+            <ol style="border: black 1px solid;position: relative;left: 15%;width: 70%;padding: 20px;">
                 <li>庫存資料來源為衛福部健保資料庫，更新頻率為90秒，若對庫存數字有疑問，可以直接詢問衛福部。</li>
                 <li>若資料呈現灰色底色者庫存可能不正確。</li>
                 <li>新增我附近的藥局功能，可以查詢方圓500公尺到3公里內的販售點。</li>
@@ -63,7 +23,7 @@
                 <li>如果您是藥局，發現備註有誤或想加入備註請直接使用藥局VPN系統裡的備註資訊。</li>
             </ol>
             <form action="" method="post">
-                <span>縣市 : </span>
+                <span class="badge badge-secondary">縣市</span>
                 <select name="city" id="city" onchange="change(this.value)" >
                     <option value="0">請選擇</option>
                     <option value="1">台北市</option>
@@ -89,55 +49,12 @@
                     <option value="21">花蓮縣</option>
                     <option value="22">連江縣</option>
                 </select>
-                <span>區域 : </span>
                 <select name="region" id="region" onchange = "region_value();">
                     <option value="0">請選擇</option>
                 </select>
-                <input type="submit" class="btn btn-secondary" name="submit" value="查詢口罩庫存">
+                <input type="submit" name="submit" value="查詢口罩庫存">
             </form>
-            <?php
-                $county = array("請選擇","台北市","基隆市","新北市","宜蘭縣","桃園市","新竹市","新竹縣","苗栗縣","台中市","彰化縣","南投縣","嘉義市","嘉義縣","雲林縣","台南市","高雄市","澎湖縣","金門縣","屏東縣","台東縣","花蓮縣","連江縣");
-                $city = $_POST["city"];
-                $region = $_POST["region"];
-                $option = isset($_POST['submit']) ? $_POST['submit'] : false;//判斷 $_POST['submit'] 是否存在,若不存在則跳到 false
-                if ($option) { //存在則列印出post值
-                    $search=search(search($arrayName,$region),$county[$city]);
-                    echo "<table class='table table-dark'>";
-                    for ($i=0; $i < count($search); $i++) { 
-                        echo "<td>";
-                        print_r($search[$i]['醫事機構代碼']);
-                        echo "</td>";
-                        echo "<td>";
-                        print_r($search[$i]['醫事機構名稱']);
-                        echo "</td>";
-                        echo "<td>";
-                        print_r($search[$i]['醫事機構地址']);
-                        echo "</td>";
-                        echo "<td>";
-                        print_r($search[$i]['醫事機構電話']);
-                        echo "</td>";
-                        echo "<td>";
-                        print_r($search[$i]['成人口罩剩餘數']);
-                        echo "</td>";
-                        echo "<td>";
-                        print_r($search[$i]['兒童口罩剩餘數']);
-                        echo "</td>";
-                        echo "<td>";
-                        print_r($search[$i]['來源資料時間']);
-                        echo "<td>";
-                        echo "<tr>";
-                    }
-                    echo "</table>";
-                
-                } else { //不存在則列印出以下內容,並結束程式
-                    echo "<p>";
-                    echo "庫存資料來源: 健保特約機構口罩剩餘數量明細清單    備註資料來源: 全民健康保險特約院所固定服務時段";
-                    echo "<br/>";
-                    echo "更新頻率: 庫存每90秒更新一次, 備註每日更新3次(08:00、14:00、18:00)";
-                    echo "</p>";
-                    exit; 
-                }
-            ?>
+
         </div>
     </div>
     <script>
@@ -177,6 +94,70 @@
     </script>
 </body>
 </html>
+<?php
+    $county = array("請選擇","台北市","基隆市","新北市","宜蘭縣","桃園市","新竹市","新竹縣","苗栗縣","台中市","彰化縣","南投縣","嘉義市","嘉義縣","雲林縣","台南市","高雄市","澎湖縣","金門縣","屏東縣","台東縣","花蓮縣","連江縣");
+    $ch = curl_init();//初始化curl 令變數為 $curlobj
+    curl_setopt($ch, CURLOPT_URL,"https://quality.data.gov.tw/dq_download_json.php?nid=116285&md5_url=2150b333756e64325bdbc4a5fd45fad1");
+    //向網址發出請求 訪問該網頁
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);//執行後不打印出來
+    //設置Cookie
+    date_default_timezone_set('Asia/Taipei'); //設置台北時區
+    curl_setopt($ch,CURLOPT_COOKIESESSION,TRUE); //設為 TRUE 時 開啟COOKIE
+    curl_setopt($ch,CURLOPT_HEADER,0);
+    curl_setopt($ch,CURLOPT_HTTPHEADER,array("content-type: application/x-www-form-urlencoded;charset=UTF-8"));
+    curl_exec($ch);//執行curl
+    $output=curl_exec($ch);  // 執行 curl 並令一個變數 $output
+    curl_close($ch); // 關閉curl
+    echo '<pre>';
+    $arrayName = json_decode($output,true);//json_decode是一個方程式 對JSON格式的字符進行解碼
 
+    $city = $_POST["city"];
+    $region = $_POST["region"];
+ 
+    var_dump($county[$city]);
 
-
+    function search($arrayName,$region){ //search方程式匯入$arrayName陣列及$region POST的值
+        $arr=$result=array();
+        foreach ($arrayName as $key => $value) { //取得$arrayName的key值
+            foreach ($value as $val) { //求出key裡面的value值
+                if(strstr($val,$region)){ //若value符合搜尋值
+                    array_push($arr,$key); //放入arr陣列
+                }
+            }
+        }
+        foreach ($arr as $key => $value) { //將$arr的key值求出
+            if(array_key_exists($value,$arrayName)){//若key值存在
+                array_push($result,$arrayName[$value]);//將key裡面的值放入array
+            }
+        }
+        return $result;
+    }
+    //var_dump(search($arrayName,$region));//呼叫search函數,json資料符合城鎮搜尋條件
+    //print_r(search(search($arrayName,$region),$county[$city]));//符合縣市搜尋條件
+?>
+<?php
+    $county = array("請選擇","台北市","基隆市","新北市","宜蘭縣","桃園市","新竹市","新竹縣","苗栗縣","台中市","彰化縣","南投縣","嘉義市","嘉義縣","雲林縣","台南市","高雄市","澎湖縣","金門縣","屏東縣","台東縣","花蓮縣","連江縣");
+    $city = $_POST["city"];
+    $region = $_POST["region"];
+    $option = isset($_POST['submit']) ? $_POST['submit'] : false;//判斷 $_POST['submit'] 是否存在,若不存在則跳到 false
+    if ($option) { //存在則列印出post值
+        $search=search(search($arrayName,$region),$county[$city]);
+        for ($i=0; $i < count($search); $i++) { 
+            print_r($search[$i]['醫事機構代碼']);
+            print_r($search[$i]['醫事機構名稱']);
+            print_r($search[$i]['醫事機構地址']);
+            print_r($search[$i]['醫事機構電話']);
+            print_r($search[$i]['成人口罩剩餘數']);
+            print_r($search[$i]['兒童口罩剩餘數']);
+            print_r($search[$i]['來源資料時間']);
+            echo "<br/>";
+        }  
+    } else { //不存在則列印出以下內容,並結束程式
+        echo "<p>";
+        echo "庫存資料來源: 健保特約機構口罩剩餘數量明細清單    備註資料來源: 全民健康保險特約院所固定服務時段";
+        echo "<br/>";
+        echo "更新頻率: 庫存每90秒更新一次, 備註每日更新3次(08:00、14:00、18:00)";
+        echo "</p>";
+        exit; 
+    }
+?>
