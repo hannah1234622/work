421d42cec53091764e6966f554322ed5fe6a8567
unknown <yu-hua@bb.com>
2020-03-06 14:55:20 +0800

    work1

--- 11.php
+++ 11.php
@@ -1,6 +1,52 @@
-<script type="text/javascript" > 
-var data="call_me_why"; 
-</script>
+<meta charset="utf-8" />
 <?php
-	echo "<script type=text/javascript>document.write(data)</script>";
+// $a=array(
+// '0' => array('id'=>1,'pid'=>0,'name'=>'水果'),
+// '1' => array('id'=>2,'pid'=>0,'name'=>'蔬菜'),
+// '2' => array('id'=>3,'pid'=>1,'name'=>'食品'),
+// '3' => array('id'=>4,'pid'=>2,'name'=>'運動'),
+// '4' => array('id'=>5,'pid'=>1,'name'=>'電腦'),
+// '5' => array('id'=>6,'pid'=>'香果','name'=>'香蕉'),
+// '6' => array('id'=>7,'pid'=>4,'name'=>'牛奶'),
+// '7' => array('id'=>8,'pid'=>5,'name'=>'西瓜'),
+// '8' => array('id'=>9,'pid'=>7,'name'=>'蘋果'),
+// );
+
+$a = array(  
+    2 => array(  
+        'catid' => 2,  
+        'catdir' => 'notice',  
+    ),  
+    5 => array(  
+        'catid' => 5,  
+        'catdir' => 'subject',  
+    ),  
+    6=> array(  
+        'catid' => 6,  
+        'catdir' => 'news'  
+    ),  
+);  
+$keywords="tice";
+
+function search($a,$keywords) {
+  $arr=$result=array();
+	foreach ($a as $key => $value) {
+		foreach ($value as $valu) {
+				if(strstr($valu, $keywords)){ 
+					array_push($arr, $key);
+				} 
+		} 
+	}
+	foreach ($arr as $key => $value) {
+				if(array_key_exists($value,$a)){
+					 array_push($result, $a[$value]);
+				}
+	}
+  return $result; 
+}
+var_dump(search($a,$keywords));
+
+
+
+//print_r($arr);
 ?>
---
+++ action.php
@@ -0,0 +1,7 @@
+
+<?php 
+
+echo $_POST["city"];
+echo $_POST["region"];
+
+?>
