b575d90fbf66a74d3110ca28e18614a07ce4e1a1
unknown <yu-hua@bb.com>
2020-03-10 17:14:30 +0800

    匯入資料庫3

--- 123.php
+++
@@ -1,3 +0,0 @@
-<?php
-ignore_user_abort();
-?>
--- action.php
+++ action.php
@@ -12,7 +12,11 @@ try {
 }
 ?>
 <?php
-    header("Content-Type:text/html;charset=utf8");
+    $county = array("請選擇","台北市","基隆市","新北市","宜蘭縣","桃園市","新竹市","新竹縣","苗栗縣","台中市","彰化縣","南投縣","嘉義市","嘉義縣","雲林縣","台南市","高雄市","澎湖縣","金門縣","屏東縣","台東縣","花蓮縣","連江縣");
+    $city = $_POST["city"];
+    $region = $_POST["region"];
+    echo $county[$city];
+    echo $region;
     $curl_handle=curl_init();//初始化curl物件
     curl_setopt($curl_handle,CURLOPT_URL,'https://data.nhi.gov.tw/resource/mask/maskdata.csv');//設定要抓取的url
     curl_setopt($curl_handle,CURLOPT_RETURNTRANSFER,true);//執行後不打印出來
